ef9216b012e96a28993963f2c9e27ae90f723cc6
Aleksandr Popovichenko <devpoal@gmail.com>
2020-10-02 23:36:57 +0300

    Updates

---
+++ precache-manifest.c73862f47f45603f8b64ff8a2cde3256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a931c539efb626811d58f644a222c246",
+    "url": "/exchange-rates/index.html"
+  },
+  {
+    "revision": "63c23e5c6a192d1cd063",
+    "url": "/exchange-rates/static/css/main.7459c197.chunk.css"
+  },
+  {
+    "revision": "bfa9d486c77773478eef",
+    "url": "/exchange-rates/static/js/2.415dfc06.chunk.js"
+  },
+  {
+    "revision": "254c01d1a3c7d8124adf4c35e46b76e3",
+    "url": "/exchange-rates/static/js/2.415dfc06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63c23e5c6a192d1cd063",
+    "url": "/exchange-rates/static/js/main.2edfa742.chunk.js"
+  },
+  {
+    "revision": "03e9a513fa28f25743d0",
+    "url": "/exchange-rates/static/js/runtime-main.d0885749.js"
+  }
+]);
